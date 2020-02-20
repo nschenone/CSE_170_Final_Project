48e929e945468c92de7b6ff198844f3effee929f
Nicholas Schenone <nschenone16@gmail.com>
2020-02-20 13:27:18 -0800

    Created pages for resources

--- routes/class.js
+++ routes/class.js
@@ -5,4 +5,31 @@ exports.viewClass = function (req, res) {
   var classQuarter = req.query.quarter;
 
   res.render('class', { "className": className, "classDescription": classDescription, "classProfessor": classProfessor, "classQuarter": classQuarter });
-};  
+};
+
+exports.viewClassTextbooks = function (req, res) {
+  var className = req.params.name;
+  var classDescription = req.query.description;
+  var classProfessor = req.query.professor;
+  var classQuarter = req.query.quarter;
+
+  res.render('textbooks', { "className": className, "classDescription": classDescription, "classProfessor": classProfessor, "classQuarter": classQuarter });
+};
+
+exports.viewClassNotes = function (req, res) {
+  var className = req.params.name;
+  var classDescription = req.query.description;
+  var classProfessor = req.query.professor;
+  var classQuarter = req.query.quarter;
+
+  res.render('classnotes', { "className": className, "classDescription": classDescription, "classProfessor": classProfessor, "classQuarter": classQuarter });
+};
+
+exports.viewClassPracticeExams = function (req, res) {
+  var className = req.params.name;
+  var classDescription = req.query.description;
+  var classProfessor = req.query.professor;
+  var classQuarter = req.query.quarter;
+
+  res.render('practiceexams', { "className": className, "classDescription": classDescription, "classProfessor": classProfessor, "classQuarter": classQuarter });
+};
